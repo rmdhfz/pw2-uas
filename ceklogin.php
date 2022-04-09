98e4b6815386875ad658cf523a206ffe9c30dc66
Hafiz Ramadhan <hfzrmd@gmail.com>
2022-04-09 11:48:54 +0700

    fix: checklogin.php

--- ceklogin.php
+++ ceklogin.php
@@ -4,12 +4,18 @@ include 'connect.php';
  
 $email = $_POST['email'];
 $password = $_POST['password'];
-$data = mysqli_query($koneksi,"select * from penjual where email='$email' and password='$password'");
+$data = mysqli_query($koneksi,"select * from penjual where email='$email'");
 $cek = mysqli_num_rows($data); 
+$res = mysqli_fetch_assoc($data);
 if($cek > 0){
-	$_SESSION['email'] = $email;
-	$_SESSION['status'] = "login";
-	header("location:/tokoonline/");
+    $verify = password_verify($password, $res['password']);
+    if ($verify) {
+        $_SESSION['email'] = $email;
+        $_SESSION['status'] = "login";
+        header("location:/tokoonline/?msg=success");
+    }else{
+        header("location:/tokoonline/?msg=failed");
+    }
 }else{
 	header("location:/tokoonline/?msg=failed");
 }
